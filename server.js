61a37517e81078836410976e0c21b9aa2dae00a2
warapatmasawat-a11y <warapatmasawat@gmail.com>
2026-04-05 21:56:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,8 @@ app.post("/analyze-durian", async (req, res) => {
   }
 });
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on http://0.0.0.0:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running");
 });
